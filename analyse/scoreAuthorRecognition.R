43e8c9fb75302172b3bd2974c72e7acc216edd65
Alex O. Holcombe <aoholcombe@gmail.com>
2018-07-28 06:58:54 +1000

    ok

---
+++ analyse/scoreAuthorRecognition.R
@@ -0,0 +1,26 @@
+authorScore<- function(js) {
+  sel<- js$selected
+  #Import the key.
+  library('readr')#install.packages('readr')
+  fileWithPath<- file.path("..","inputFiles","authorsAndNonauthors.txt")
+  key <-read_tsv(fileWithPath)  #read_tsv from tidyverse readr has advantage of not having padding spaces
+  
+  #Calculate how many authors selected.
+  numTested <- length(sel)
+  #get scoring key only for those tested, which is the first numTested of the whole key
+  keyThis<- key$author[1:numTested]
+  selWithAns <- data.frame(sel,keyThis)
+  #Use dplyr to select rows where keyThis ==1, then add up number selected
+  library(dplyr)
+  authors<- selWithAns %>% filter(keyThis==1)
+  maxScorePossible <- nrow(authors)
+  
+  authorsChosen<- sum(authors$sel)
+  
+  #Calculate how many non-authors selected
+  nonAuthors<- selWithAns %>% filter(keyThis==0)
+  nonAuthorsChosen <- sum(nonAuthors)
+  scoreRaw <- authorsChosen - nonAuthorsChosen
+  scorePct <- scoreRaw / maxScorePossible
+  return("authorsRaw" = scoreRaw, "authorsPct" = authorsPct, "authorsTotalChosen"=sum(sel))
+}
